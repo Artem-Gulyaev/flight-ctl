58aa834501e0c3947a08c20c21445ffbb2036662
Artem Gulyaev <gulyaev.artem.a@gmail.com>
2021-02-19 01:28:50 +0100

    [Pendulum] Adjusted energy correction
    * now it is working that way: we first
      put Hamiltonian equations in work, and then
      we see the generalized forces work and we
      adjust the dp and dq in such a way, that
      the work of external forces exactly equals
      to the loss/gain of the total system energy.
    * current flaw is that energy optimization is now
      independent on the Hamiltonian eqations, while
      we need to optimize them simultaneously.

--- down_pendulum/down_pendulum.py
+++ down_pendulum/down_pendulum.py
@@ -16,18 +16,22 @@ class Pendulum:
 
     # RETURNS: default initial state for the system
     def default_init_state():
-        phase_vector = np.ndarray(shape=(1,2), dtype=float)
-        # alpha
+        N_vars = 1
+        phase_vector = np.ndarray(shape=(N_vars,3), dtype=float)
+        # q1: alpha
         phase_vector[0][0] = 1.0
-        # alpha momentum
+        # p1: alpha momentum
         phase_vector[0][1] = 0.0
+        # f1: alpha generalized force (external wrt the system)
+        phase_vector[0][2] = 0.0
         return phase_vector
 
     # @self.phase_vector a phase space vector
     #   which describes the system state
     #   NOTE: first index selects the generalized variable
     #         second index == 0 selects the generalized coordinate
-    #                      == 1 selects the corresponding moment
+    #                      == 1 selects the corresponding generalized momentum
+    #                      == 2 selects the corresponding external force
     def __init__(self, params=None, init_phase_vector=None):
         if params is None:
             self.params = Pendulum.default_params()
@@ -47,10 +51,12 @@ class Pendulum:
     def __str__(self):
         return "Phase vector: %s\nParameters: %s\nHamiltonian: %s\n" % (str(self.phase_vector), str(self.params), str(self.H()))
 
-    # Hamiltonian of the system
-    # TODO: take it into account at computing to adjust
-    #    phase vector to keep H constant (surely in case of
-    #    no external forces)
+    # Hamiltonian of the system (dynamical external forces are ignored
+    # here, cause random H shift which comes from current forces
+    # being used in computation gives us just nothing, cause its
+    # derivatives only matter)
+    # NOTE: meanwhile the forces are to be computed for
+    #   Hamiltonian derivatives.
     def H(self):
         m = self.params["m_kg"]
         g = self.params["g_mps2"]
@@ -68,7 +74,13 @@ class Pendulum:
             g = self.params["g_mps2"]
             l = self.params["l_m"]
             q_alpha = self.phase_vector[0][0]
-            return m * g * l * sin(q_alpha)
+            f_alpha = self.phase_vector[0][2]
+
+            #                                    Immediate
+            #             Internal               Arbitrary
+            #       ----Conservative-----      -External-
+            #      /       force         \    /  force /
+            return m * g * l * sin(q_alpha) + f_alpha
         raise RuntimeError("Unknown variable index: %s" % str(var_idx))
 
     # Partial derivative of our Hamiltonian
@@ -85,6 +97,12 @@ class Pendulum:
     # given external generalized forces on the system.
     # TODO: take into account generalized forces
     def step(self, eneralized_forces_vector, dt):
+        # initial energy at the beginning of the step
+        H1 = self.H()
+
+        # first to columns are dp and dq
+        # last column is the work of external forces
+        dV = np.ndarray(shape=self.phase_vector.shape, dtype=float)
         for i in range(self.phase_vector.shape[0]):
             # Hamilton equations for evolution
             # dq/dt = dH/dp
@@ -93,18 +111,34 @@ class Pendulum:
             # dq = (dH/dp) * dt
             # dp = -(dH/dq) * dt
 
-            dp = - self.dHdq(i) * dt
+            # first we jump directly to raw solution, which
+            # is not optimized due to finite dt, and might have
+            # energy discrepancy, we use it as 0th-approximation
+            # to correct solution
+            dp = -self.dHdq(i) * dt
             dq = self.dHdp(i) * dt
 
-            self.phase_vector[i, 0] += dq
-            self.phase_vector[i, 1] += dp
+            # preliminary change of the phase vector
+            dV[i, 0] = dq
+            dV[i, 1] = dp
+
+            dV[i, 2] = dq * self.phase_vector[i,2]
+
+        # updating all q and p
+        self.phase_vector[:,0] += self.phase_vector[:,0] + dV[:,0]
+        self.phase_vector[:,1] += self.phase_vector[:,1] + dV[:,1]
+
+        # computing energy gain/loss due to external forces
+        # work along/against the system
+        H_estimated = H1 + np.sum(dV[:,2])
 
         # for now fits only to the conservative systems
-        self.apply_H_correction()
+        self.apply_H_correction(H_estimated)
 
     # corrects the current phase space location to
-    # fit H=const constraint
-    def apply_H_correction(self):
+    # fit H=H_estimated constraint using the gradient descent
+    # with decay
+    def apply_H_correction(self, H_estimated):
         # how much of grad is determined with the current grad
         grad_decay = 0.1
         # how big step wrt to grad
@@ -113,15 +147,15 @@ class Pendulum:
         H_epsilon = 0.00001
 
         def loss():
-            return (self.H() - self.H0)**2
+            return (self.H() - H_estimated)**2
 
         # @i the coordinate variable index
         def dLdq(i):
-            return 2 * (self.H() - self.H0) * self.dHdq(i)
+            return 2 * (self.H() - H_estimated) * self.dHdq(i)
 
         # @i the momentum variable index
         def dLdp(i):
-            return 2 * (self.H() - self.H0) * self.dHdp(i)
+            return 2 * (self.H() - H_estimated) * self.dHdp(i)
         
         # initializing the gradient vector with initial grad
         gradient = np.ndarray(shape=self.phase_vector.shape, dtype=float)
@@ -129,7 +163,8 @@ class Pendulum:
             gradient[i,0] = self.dHdq(i)
             gradient[i,1] = self.dHdp(i)
 
-        # iterating over down to min of loss function
+        # iterating over down to min of loss function with decaying
+        # gradient descent variation
         steps_counter = 0
         while loss() > H_epsilon:
             gradient = np.ndarray(shape=self.phase_vector.shape, dtype=float)
